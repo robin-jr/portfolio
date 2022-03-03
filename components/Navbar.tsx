831b682dceef4652d78583df8f28ffc5453f42fc
Robin J <robin__j@outlook.com>
2022-03-03 13:59:35 +0530

    Navbar done

---
+++ components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className="fixed gap-5 font-light flex justify-end text-lg p-3 bg-gray-900 w-full bg-opacity-80">
+      <div>Home</div>
+      <div>About</div>
+      <div>Projects</div>
+    </div>
+  )
+}
+
+export default Navbar
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,21 @@
 import Head from 'next/head'
 import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex min-h-screen flex-col pb-2">
       <Head>
         <title>Robin J</title>
         <meta name="description" content="My portfolio website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Navbar/>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
       </main>
 
       <Footer />
-
     </div>
   )
 }
