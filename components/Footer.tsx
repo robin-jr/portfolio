0834b65a0f56a7a55c70829ad714527e3466b750
Robin J <robin__j@outlook.com>
2022-03-03 09:31:45 +0530

    footer done

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer className="flex h-24 w-full items-center justify-center font-comic color-secondary">
+            <p className="text-center">Designed & built by <br />Robin J </p>
+        </footer>
+    )
+}
+
+export default Footer
