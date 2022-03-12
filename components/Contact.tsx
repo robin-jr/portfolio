c84398b4247950b481b6737288db62b4f0e366e8
Robin J <robin__j@outlook.com>
2022-03-12 12:23:49 +0530

    added social icons

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -5,7 +5,7 @@ function Contact() {
     <div className="text-center">
         <div className="font-comic color-secondary">What's next?</div>
         <div className="text-5xl mt-2">Get in Touch</div>
-        <a href="mailto:robinjeyasingh@outlook.com" className="button mt-20 inline-block">Say Hello</a>
+        <a href="mailto:robinjeyasingh@outlook.com" target="_blank" className="button mt-20 inline-block">Say Hello</a>
     </div>
   )
 }
--- components/Lines.tsx
+++ components/Lines.tsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import styles from "../styles/Lines.module.scss"
+import Image from 'next/image'
 
 function Lines() {
     return (
         <>
             <div className={styles.leftLine}>
                 <div className="wrapper">
-                    <div><span className="material-icons">&#xe867;</span></div>
-                    <div><span className="material-icons">&#xe867;</span></div>
-                    <div><span className="material-icons">&#xe867;</span></div>
+                    <a href="https://www.github.com/robin-jr/" target="_blank">
+                        <Image className="icon" color="red" src="/github.svg" height={30} width={30} />
+                    </a>
+                    <a href="mailto:robinjeyasingh@outlook.com" target="_blank"><span className="material-icons">&#xe0e1;</span></a>
+                    <a href="https://www.linkedin.com/in/robinjeyasingh/" target="_blank">
+                        <Image className="icon" color="red" src="/linkedin.svg" height={30} width={30} />
+                    </a>
                 </div>
             </div>
             <div className={styles.rightLine}>
-                <a href="mailto:robinjeyasingh@outlook.com">
+                <a href="mailto:robinjeyasingh@outlook.com" target="_blank">
                     robinjeyasingh@outlook.com
                 </a>
             </div>
