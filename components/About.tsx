2cc5eda56e353b7f4db07bc0378fb7691f373247
Robin J <robin__j@outlook.com>
2022-03-12 17:53:53 +0530

    added robin's photo

--- components/About.tsx
+++ components/About.tsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import Timeline from '../widgets/Timeline'
-import TitleAndBadges, { Entry } from '../widgets/TitleAndBadges'
+import TitleAndBadges from '../widgets/TitleAndBadges'
 
 function About() {
     return (
         <div className="flex w-[80%]">
             <div className="flex flex-col flex-1 mr-10 items-center">
-                <img src="https://www.picsum.photos/1080/1920" className="h-[40em] w-[30em] mb-10"></img>
+                <div className="relative">
+                    <img src={"/robin-color.jpg"} className="w-[30em] mb-10"></img>
+                    <img src={"/robin-grey.jpg"} className="w-[30em] mb-10 transition-all duration-700 absolute top-0 hover:opacity-0"></img>
+                </div>
                 <div className="text-lg text-center">Friendly neighbourhood<br />Programmer 👋</div>
             </div>
             <div className="flex flex-col flex-1">
                 <div className="text-3xl color-tertiary mb-4">Things I Know</div>
-                <TitleAndBadges/>
+                <TitleAndBadges />
                 <div className="text-3xl color-tertiary mb-4 mt-10">My Journey</div>
-                <Timeline/>
+                <Timeline />
             </div>
         </div>
     )
